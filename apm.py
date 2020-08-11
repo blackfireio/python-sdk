e8c03bc9a2efc461c90ba1fda1cba3f5c875b002
Sumer Cip <sumerc@gmail.com>
2020-08-11 11:57:11 +0300

    change version&changelog + rm APM code and skip related tests

--- apm.py
+++ apm.py
@@ -120,89 +120,11 @@ def trigger_extended_trace():
 
 
 def _send_trace_async(data):
-    agent_conn = agent.Connection(
-        _apm_probe_config.agent_socket, _apm_probe_config.agent_timeout
-    )
-
-    try:
-        agent_conn.connect()
-        agent_conn.send(data)
-
-        # verify agent responds success
-        response_raw = agent_conn.recv()
-        agent_resp = agent.BlackfireAPMResponse().from_bytes(response_raw)
-        if 'false' in agent_resp.status_val_dict['success']:
-            raise BlackfireAPMException(agent_resp.status_val_dict['error'])
-
-        # Update config if any configuration update received
-        if len(agent_resp.args) or len(agent_resp.key_pages):
-            new_apm_config = ApmConfig()
-            try:
-                new_apm_config.sample_rate = float(
-                    agent_resp.args['sample-rate'][0]
-                )
-            except:
-                pass
-            try:
-                new_apm_config.extended_sample_rate = float(
-                    agent_resp.args['extended-sample-rate'][0]
-                )
-            except:
-                pass
-
-            new_apm_config.key_pages = tuple(agent_resp.key_pages)
-
-            # update the process-wise global apm configuration. Once this is set
-            # the new HTTP requests making initialize() will get this new config
-            _bfext.set_ext_data("apm_config", new_apm_config)
-
-        if log.isEnabledFor(logging.DEBUG):
-            log.debug(
-                "APM trace sent. [%s]",
-                json_prettify(data),
-            )
-
-    except Exception as e:
-        if is_testing():
-            raise e
-        log.error("APM message could not be sent. [reason:%s]" % (e))
-    finally:
-        agent_conn.close()
+    return
 
 
 def send_trace(request, **kwargs):
-    global _runtime_metrics
-
-    try:
-        data = """file-format: BlackfireApm
-            sample-rate: {}
-        """.format(_apm_config.sample_rate)
-
-        # add extra headers
-        mu, pmu = _runtime_metrics.memory()
-        kwargs["mu"] = mu
-        kwargs["pmu"] = pmu
-
-        for k, v in kwargs.items():
-            if v:
-                # convert `_` to `-` in keys. e.g: controller_name -> controller-name
-                k = k.replace('_', '-')
-                data += "%s: %s\n" % (k, v)
-
-        # add final marker
-        data += "\n"
-
-        if IS_PY3:
-            data = bytes(data, 'ascii')
-
-        # We should not have a blocking call in APM path. Do agent connection setup
-        # socket send in a separate thread.
-        run_in_thread_pool(_send_trace_async, args=(data, ))
-    except Exception as e:
-        if is_testing():
-            raise e
-
-        log.exception(e)
+    return
 
 
 def send_extended_trace(request, **kwargs):
