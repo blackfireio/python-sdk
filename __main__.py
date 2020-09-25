75d55038df2ed628a67f5b7b06c70063b6f39c67
Sumer Cip <sumerc@gmail.com>
2020-09-25 17:23:08 +0300

    remove install/uninstall bootstrap and define deprecation msg

--- __main__.py
+++ __main__.py
@@ -1,34 +1,10 @@
-import os
 import sys
-from distutils.sysconfig import get_python_lib
-from blackfire.utils import console_input
-from blackfire import _install_bootstrap, _uninstall_bootstrap
+from blackfire.utils import console_input, get_logger
 
+log = get_logger(__name__)
 
-def hello_world():
-    print(
-        '\nHello! Please do not mess with this complex function. You are warned!\n'
+cmd = sys.argv[1]
+if cmd == 'install-bootstrap' or cmd == 'uninstall-bootstrap':
+    log.warning(
+        'DeprecationWarning: Do not use this command, use `blackfire-python` instead.'
     )
-
-
-if len(sys.argv) > 1:
-    cmd = sys.argv[1]
-    if cmd == 'install-bootstrap':
-        q = "This command will install Python pre-interpreter hook files in %s. " \
-            "By installing this pre-interpreter hook, you will be able to use " \
-            "`blackfire run` without any change to your code. Learn more at " \
-            "`https://blackfire.io/docs`.\n\nDo you confirm installation? [Y/n]: " % \
-            (get_python_lib())
-        r = console_input(q).lower().strip()
-        if not r or r == 'y':
-            _install_bootstrap()
-    elif cmd == 'uninstall-bootstrap':
-        q = "This command will uninstall Python pre-interpreter hook files." \
-            "\n\nDo you confirm? [y/N]: "
-        r = console_input(q).lower().strip()
-        if r == 'y':
-            _uninstall_bootstrap()
-    elif cmd == 'hello-world':
-        hello_world()
-    else:
-        raise Exception('There is no such command: (%s)' % (cmd))
