6da5e3367463ac1236c2e3ee0f19c41ec48428b2
Sümer Cip <sumerc@gmail.com>
2020-11-12 13:23:42 +0300

    import custom sitecustomize if there is any

--- bootstrap/sitecustomize.py
+++ bootstrap/sitecustomize.py
@@ -1,3 +1,25 @@
+import os
+import sys
+import imp
 import blackfire
 
+from blackfire.utils import get_logger
+
+log = get_logger(__name__)
+
 blackfire.bootstrap()
+
+# Ensure other sitecustomize.py is called if available in sys.path
+bootstrap_dir = os.path.dirname(__file__)
+path = list(sys.path)
+
+if bootstrap_dir in path:
+    path.remove(bootstrap_dir)
+
+try:
+    (f, path, description) = imp.find_module("sitecustomize", path)
+except ImportError:
+    pass
+else:
+    log.debug("sitecustomize from user found in: %s", path)
+    imp.load_module("sitecustomize", f, path, description)
