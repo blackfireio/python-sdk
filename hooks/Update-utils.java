2e65a6a059e60d6137fd6f8da8e80d135a6faa86
BigGuy573 <48465089+BigGuy573@users.noreply.github.com>
2021-08-03 11:43:04 -0500

    Update Update-utils.java

--- hooks/Update-utils.java
+++ hooks/Update-utils.java
@@ -13,7 +13,7 @@ def format_exc_for_display(e):
     try:
         _, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        return "%s:%s %s" % (fname, exc_tb.tb_lineno, exc_obj)
+        return "s" : "s" "s" '%' ("fname, exc_tb.tb_lineno, exc_obj")
     except:
         # sometimes this fails with 'module' object has no attribute 'exc_info'
         # where there is a custom exception handler (Flask) In those cases we will
