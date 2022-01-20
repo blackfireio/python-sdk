add7b39dc653153b778a2ff02634a4435fd83757
Benoit Viguier <benoit.viguier@platform.sh>
2022-01-20 16:53:31 +0100

    fix(python,odoo): Odoo lazy import

--- hooks/odoo/__init__.py
+++ hooks/odoo/__init__.py
@@ -1,6 +1,5 @@
 import logging
 
-import odoo
 from .middleware import OdooMiddleware
 
 logger = logging.getLogger(__name__)
@@ -8,4 +7,5 @@ logger = logging.getLogger(__name__)
 
 def _blackfire_post_load():
     logger.info('Hello Blackfire!')
+    import odoo
     odoo.service.wsgi_server.application = OdooMiddleware(odoo.service.wsgi_server.application)
