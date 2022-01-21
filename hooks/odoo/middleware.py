3099a02a169bfe10096cd884c464777b38851d7a
Sümer Cip <sumer.cip@platform.sh>
2022-01-21 17:31:20 +0300

    Refactored the start_response and removed the necessity of mutating headers

--- hooks/odoo/middleware.py
+++ hooks/odoo/middleware.py
@@ -3,81 +3,94 @@ from blackfire.utils import get_logger, read_blackfireyml_content
 
 logger = get_logger(__name__)
 
+# class OdooMiddleware(object):
+#     def __init__(self, application):
+#         self.application = application
+#         print("odo mid loaded...")
+
+#     def __call__(self, environ, start_response):
+#         # from blackfire import profiler
+
+#         # profiler.start()
+
+#         response = self.application(environ, start_response)
+        
+#         print(">>>>")
+#         print(type(response), dir(response))
+#         print(">>>>")
+
+#         # profiler.stop()
+#         # print(profiler.get_traces())
+
+def _extract_headers(headers):
+    return dict((k, v) for (k, v) in headers)
+
+
 class OdooMiddleware(object):
     def __init__(self, application):
         self.application = application
 
     def __call__(self, environ, start_response):
-        if 'HTTP_X_BLACKFIRE_QUERY' not in environ:
-            logger.debug('Blackfire: PASSTHROUGH')
-            return self.application(environ, start_response)
+        # TODO: bfyaml, APM
 
-        # TODO: Get .blackfire.yaml when applicable
-        logger.debug('Blackfire: PROFILE')
-        return self._blackfired_request(environ, start_response)
+        # profile?
+        if 'HTTP_X_BLACKFIRE_QUERY' in environ:
+            return self._blackfired_request(environ, start_response)
 
-    def _get_query_from_environ(self, environ):
-        return environ['HTTP_X_BLACKFIRE_QUERY']
+        self.application(environ, start_response)
+
 
     def _blackfired_request(self, environ, start_response):
-        query = self._get_query_from_environ(environ)
+        query = environ['HTTP_X_BLACKFIRE_QUERY']
         logger.debug(
             "OdooMiddleware._blackfired_request called. [query=%s]", query
         )
 
-        new_probe = None
+        content_length = status_code = None
+        probe_err, probe = try_enable_probe(query)
         try:
-            bf_status = None
-            bf_headers = None
-
             def _start_response(status, headers):
-                nonlocal bf_status, bf_headers
-                bf_status = status
-                bf_headers = headers
-                resp = start_response(status, headers)
+                nonlocal status_code, content_length, probe_err, probe
 
-                return resp
+                try:
+                    status_code = int(status[:3]) # e.g. 200 OK
+                except Exception as e:
+                    logger.exception(e)
+                headers_dict = _extract_headers(headers)
+                content_length = headers_dict.get('Content-Length')
 
-            probe_err, new_probe = try_enable_probe(query)
-            # let exceptions propagate through
+                if probe:
+                    if probe_err:
+                        headers.append((probe_err[0], probe_err[1]))
+                    else:
+                        headers.append(('X-Blackfire-Response',
+                            probe.get_agent_prolog_response().status_val)
+                        )
+
+                return start_response(status, headers)
+
             response = self.application(environ, _start_response)
-
-            if probe_err:
-                logger.info(probe_err)
-                bf_headers.append((probe_err[0], probe_err[1]))
-            else:
-                response_len = -1
-                for h in bf_headers:
-                    if h[0] == 'Content-Length':
-                        response_len = h[1]
-                        break
-
-                probe_resp = try_end_probe(
-                    new_probe,
-                    response_status_code=bf_status[0:3],  # status contains code+string status, e.g. "200 OK"
-                    response_len=response_len,
-                    controller_name=environ['REQUEST_URI'],
-                    framework="odoo",
-                    http_method=environ['REQUEST_METHOD'],
-                    http_uri=environ['REQUEST_URI'],
-                    https='1' if environ['wsgi.url_scheme'] == 'https' else '',
-                    http_server_addr=environ['SERVER_NAME'],
-                    http_server_software=environ['SERVER_SOFTWARE'],
-                    http_server_port=environ['SERVER_PORT'],
-                    http_header_host=environ['HTTP_HOST'],
-                    http_header_user_agent=environ['HTTP_USER_AGENT'],
-                    http_header_x_forwarded_host=environ['HTTP_X_FORWARDED_HOST'] if 'HTTP_X_FORWARDED_HOST' in environ else '',
-                    http_header_x_forwarded_proto=environ['HTTP_X_FORWARDED_PROTO'] if 'HTTP_X_FORWARDED_PROTO' in environ else '',
-                    http_header_x_forwarded_port=environ['HTTP_X_FORWARDED_PORT'] if 'HTTP_X_FORWARDED_PORT' in environ else '',
-                    http_header_forwarded=environ['HTTP_FORWARDED'] if 'HTTP_FORWARDED' in environ else '',
-                )
-                bf_headers.append((probe_resp[0], probe_resp[1]))
-
             return response
 
         finally:
             logger.debug("OdooMiddleware._profiled_request ended.")
 
-            if new_probe:
-                new_probe.disable()
-                new_probe.clear_traces()
+            probe_resp = try_end_probe(
+                probe,
+                response_status_code=status_code,
+                response_len=content_length,
+                controller_name=environ['REQUEST_URI'],
+                framework="odoo",
+                http_method=environ['REQUEST_METHOD'],
+                http_uri=environ['REQUEST_URI'],
+                https='1' if environ['wsgi.url_scheme'] == 'https' else '',
+                http_server_addr=environ['SERVER_NAME'],
+                http_server_software=environ['SERVER_SOFTWARE'],
+                http_server_port=environ['SERVER_PORT'],
+                http_header_host=environ['HTTP_HOST'],
+                http_header_user_agent=environ['HTTP_USER_AGENT'],
+                http_header_x_forwarded_host=environ['HTTP_X_FORWARDED_HOST'] if 'HTTP_X_FORWARDED_HOST' in environ else '',
+                http_header_x_forwarded_proto=environ['HTTP_X_FORWARDED_PROTO'] if 'HTTP_X_FORWARDED_PROTO' in environ else '',
+                http_header_x_forwarded_port=environ['HTTP_X_FORWARDED_PORT'] if 'HTTP_X_FORWARDED_PORT' in environ else '',
+                http_header_forwarded=environ['HTTP_FORWARDED'] if 'HTTP_FORWARDED' in environ else '',
+            )
