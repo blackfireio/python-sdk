ef5017f4176f831fe1bb7234f0473a11a078dbbd
Sümer Cip <sumer.cip@platform.sh>
2022-01-28 12:33:44 +0300

    Change debug log text

--- hooks/odoo/__init__.py
+++ hooks/odoo/__init__.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 
 def _blackfire_post_load():
-    logger.debug('Hello Blackfire!')
+    logger.debug('Odoo._blackfire_post_load called.')
     import odoo
     odoo.service.wsgi_server.application = OdooMiddleware(
         odoo.service.wsgi_server.application
