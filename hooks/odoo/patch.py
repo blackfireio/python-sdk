5708987725c331dcca635e93bf6423c1f697a9a2
Sümer Cip <sumer.cip@platform.sh>
2022-03-07 13:21:03 +0300

    Defensive chk for sys.argv in Odoo

--- hooks/odoo/patch.py
+++ hooks/odoo/patch.py
@@ -1,20 +1,25 @@
-from blackfire.utils import wrap, import_module, get_logger, get_executable_path
+import sys
+from blackfire.utils import import_module, get_logger, get_executable_path
 
 log = get_logger(__name__)
 
 
 def patch():
 
-    # Since Odoo is not importable by default, try to add
-    # its directory in python path.
-    # Assumes that odoo-bin is in the same directory than odoo module.
-    import sys
-    executable_path = get_executable_path(sys.argv[0])
-    if executable_path is not None and executable_path.endswith('/odoo-bin'):
-        import os
-        odoo_path = os.path.dirname(executable_path)
-        log.debug("Detected Odoo path: %s", odoo_path)
-        sys.path.append(odoo_path)
+    # this defensive check is necessary since sometimes argv is not present
+    # during bootstrap
+    if hasattr(sys, 'argv'):
+        # Since Odoo is not importable by default, try to add
+        # its directory in python path.
+        # Assumes that odoo-bin is in the same directory than odoo module.
+        executable_path = get_executable_path(sys.argv[0])
+        if executable_path is not None and executable_path.endswith(
+            '/odoo-bin'
+        ):
+            import os
+            odoo_path = os.path.dirname(executable_path)
+            log.debug("Detected Odoo path: %s", odoo_path)
+            sys.path.append(odoo_path)
 
     module = import_module('odoo')
     if not module:
