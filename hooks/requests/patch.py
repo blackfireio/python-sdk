704760806990ceccc7878885689b4538451b5991
Sümer Cip <sumer.cip@platform.sh>
2022-11-18 12:42:38 +0300

    Add tests for monkey patch and http naming

--- hooks/requests/patch.py
+++ hooks/requests/patch.py
@@ -6,14 +6,17 @@ log = get_logger(__name__)
 
 
 def _wrap_send(fn, self, request, **kwargs):
-    bf_http_title = request.headers.get('X-Blackfire-HTTP-Query-Title')
+    try:
+        bf_http_title = request.headers.get('X-Blackfire-HTTP-Query-Title')
 
-    if bf_http_title is not None:
-        with profiler.start_pending_span(
-            name=bf_http_title, fn_name='requests.sessions.Session.send'
-        ) as span:
-            r = fn(self, request, **kwargs)
-            return r
+        if bf_http_title is not None:
+            with profiler.start_pending_span(
+                name=bf_http_title, fn_name='requests.sessions.Session.send'
+            ) as span:
+                r = fn(self, request, **kwargs)
+                return r
+    except Exception as e:
+        log.exception(e)  # no matter what, call the orig. fn
 
     return fn(self, request, **kwargs)
 
